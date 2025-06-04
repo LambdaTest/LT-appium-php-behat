e8bda66573dd07b2570178c4f21b558406564fe8
UchihaMadaraaa <karang@lambdatest.com>
2025-06-04 15:00:10 +0530

    TTN-23587 Minor changes to make the tests executable

--- lib/LambdaContext.php
+++ lib/LambdaContext.php
@@ -2,6 +2,9 @@
 
 require 'vendor/autoload.php';
 
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+
 class LambdaContext implements Behat\Behat\Context\Context
 {
     protected static $CONFIG;
@@ -9,46 +12,75 @@ class LambdaContext implements Behat\Behat\Context\Context
     protected static $LTUSER;
     protected static $LTKEY;
 
+    private static function log($msg) {
+        echo "[" . date('Y-m-d H:i:s') . "] $msg\n";
+    }
+
     public function __construct($parameters){
+        self::log("Initializing LambdaContext with parameters:");
+        print_r($parameters);
+        
         self::$CONFIG = $parameters;
+        self::$LTUSER = $parameters['user'];
+        self::$LTKEY = $parameters['key'];
 
-        self::$LTUSER = getenv('LT_USERNAME');
-        if(!self::$LTUSER) self::$LTUSER = $GLOBALS['CONFIG']['user'];
-
-        self::$LTKEY = getenv('LT_ACCESS_KEY');
-        if(!self::$LTKEY) self::$LTKEY = $GLOBALS['CONFIG']['key'];
+        self::log("Using credentials:");
+        self::log("Username: " . self::$LTUSER);
+        self::log("Server: " . self::$CONFIG['server']);
+        self::log("PHP Version: " . phpversion());
+        self::log("Environment Variables:");
+        print_r($_ENV);
+        self::log("getenv('LT_USERNAME'): " . getenv('LT_USERNAME'));
+        self::log("getenv('LT_ACCESS_KEY'): " . getenv('LT_ACCESS_KEY'));
 
         // Check if driver has been created, if false then create it
         if( !self::$driver ) {
+            self::log("Creating new driver instance");
             self::createDriver();
         }
     }
 
     public static function createDriver() {
-
-        # Each parallel test running will contain test run id
+        try {
         $test_run_id = getenv("TEST_RUN_ID") ? getenv("TEST_RUN_ID") : 0; 
-        
-        # hub URL for LT
         $url = "http://" . self::$LTUSER . ":" . self::$LTKEY . "@" . self::$CONFIG['server'] .":80/wd/hub";
+            self::log("Hub URL: $url");
 
-        # get the capabilities for this test_run_id 
-        # Capabilities contains the Platform, Browser
         $caps = self::$CONFIG["environments"][$test_run_id];
-        # fetch capabilities that we need to apply for each test
+            self::log("Environment capabilities:");
+            print_r($caps);
+
         foreach (self::$CONFIG["capabilities"] as $capsName => $capsValue) {
-            if(!array_key_exists($capsName, $caps))
+                if(!array_key_exists($capsName, $caps)) {
                 $caps[$capsName] = $capsValue;
         }
+            }
+            self::log("Final capabilities after merge:");
+            print_r($caps);
 
-        self::$driver = RemoteWebDriver::create($url, $caps, 120000, 120000);
+            self::log("Attempting to create RemoteWebDriver...");
+            // Try to log the request payload
+            self::log("Request payload to Appium:");
+            print_r([
+                'url' => $url,
+                'capabilities' => $caps
+            ]);
+            self::$driver = RemoteWebDriver::create($url, $caps, 120000, 120000);
+            self::log("RemoteWebDriver created successfully");
+        } catch (Exception $e) {
+            self::log("[ERROR] Failed to create driver: " . $e->getMessage());
+            self::log("[ERROR] Stack trace:\n" . $e->getTraceAsString());
+            throw $e;
+        }
     }
 
     /** @AfterFeature */
     public static function tearDown()
     {
-        if(self::$driver)
+        if(self::$driver) {
+            self::log("Quitting driver");
         self::$driver->quit();
+        }
     }
 }
 ?>
